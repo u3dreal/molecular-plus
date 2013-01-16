5582f8622c750ff10a27232f2cc871151cf8d65c
JF <pyroevil@hotmail.com>
2013-01-16 08:24:52 -0500

    kdtree integrated
    not working well for now. need to fix it. seem to not evaluate distance
    correctly during a query. A lot of print are added in this commit to try
    to fix the problem.
    Old hashgrid are deleted

--- molecular/molcore.py
+++ molecular/molcore.py
@@ -1,5 +1,7 @@
 #print("allo")
 from math import floor
+from sys import getsizeof
+from time import clock
 
 def init(importdata):
     global fps
@@ -7,18 +9,38 @@ def init(importdata):
     global psys
     global hashgrid
     
+    listsqsize = []
     fps = importdata[0][0]
     substep = importdata[0][1]
     #print(fps,substep)
     psys = []
-    hashgrid = HashTree()
+    hashgrid = KDTree()
     for i in importdata[1:]:
         psys.append(ParSys(i))
     parnum = 0
     for i in psys:
         parnum += i.parnum
-    print(hashgrid.sizelist)
-    print(hashgrid.query(psys[0].particle[11]))
+        listsqsize += i.sqsize_list
+    listsqsize = list(set(listsqsize))
+    listsqsize.reverse()
+    parlist = []  
+    for i in psys:
+        for ii in i.particle:
+            parlist.append(ii)
+    timer = clock()
+    kdtree = KDTree()
+    kdtree.create(parlist,"root")
+    print("  KDtree generation take:",round(clock()-timer,5),"sec")
+    point = (0,0,0)
+    r = 0.2
+    timer = clock()
+    query = kdtree.rnn_query(point,r)
+    print("  RNN query number find:",len(query))
+    print("  KDtree RNN query take:",round(clock()-timer,5),"sec")
+    timer = clock()
+    query = kdtree.brute_query(point,r)
+    print("  Brute query number find:",len(query))
+    print("  Brute query take:",round(clock()-timer,5),"sec")
     return parnum
     
 
@@ -28,6 +50,7 @@ class ParSys:
         
         self.parnum = data[0]
         self.particle = [0] * self.parnum
+        self.sqsize_list = []
         self.selfcollision_active = data[6][0]
         self.othercollision_active = data[6][1]
         self.collision_group = data[6][2]
@@ -59,10 +82,11 @@ class ParSys:
             self.particle[i].vel = data[2][(i * 3):(i * 3 + 3)]
             self.particle[i].size = data[3][i]
             self.particle[i].sqsize = sq_number(self.particle[i].size)
+            if self.particle[i].sqsize not in self.sqsize_list:
+                self.sqsize_list.append(self.particle[i].sqsize)
             self.particle[i].mass = data[4][i]
             self.particle[i].state = data[5][i]
             self.particle[i].sys = self
-            hashgrid.add_point(self.particle[i])
             #print(self.particle[i].size,self.particle[i].sqsize)
             
         
@@ -76,98 +100,81 @@ class Particle(ParSys):
         self.mass = "is mass"
         self.state = "is alive state"
         self.sys = "is parent system"
- 
-       
-class HashTree:
-    def __init__(self):
-        self.sizelist = []
-        self.tree={}
-        self.ngrid = []
-        rrange = 1
-        for xgrid in range(-rrange,rrange + 1):
-            for ygrid in range(-rrange,rrange + 1):
-                for zgrid in range(-rrange,rrange + 1):
-                    self.ngrid.append((xgrid,ygrid,zgrid))
-        #print(self.ngrid)
-        #print(len(self.ngrid))
-        #print("Gridsize at:",self.gridsize)
-       
-    def add_point(self,mol):
-        x = mol.loc[0]
-        y = mol.loc[1]
-        z = mol.loc[2]
-        size = mol.sqsize 
-        if size not in self.sizelist:
-            self.sizelist.append(size)
-        intx = floor(x / size)
-        inty = floor(y / size)
-        intz = floor(z / size)
-        if (size,intx,inty,intz) not in self.tree:
-            self.tree[size,intx,inty,intz] = []
-        if mol not in self.tree[size,intx,inty,intz]:
-            self.tree[size,intx,inty,intz].append((mol.id,mol.sqsize))
-            #print (x,y,z,";",intx,inty,intz)
-           
-    def remove_point(self,mol):
-        x = mol.loc[0]
-        y = mol.loc[1]
-        z = mol.loc[2]
-        size = mol.sqsize
-        intx = floor(x / size)
-        inty = floor(y / size)
-        intz = floor(z / size)
-        if (size,intx,inty,intz) in self.tree:
-            for i in range((self.tree[size,intx,inty,intz].count(mol))):
-                if (size,intx,inty,intz) in self.tree:
-                    self.tree[size,intx,inty,intz].remove(mol)
-                       
-    def update(self,prev_loc,mol):
-        x = mol.loc[0]
-        y = mol.loc[1]
-        z = mol.loc[2]
-        px = prev_loc[0]
-        py = prev_loc[1]
-        pz = prev_loc[2]
-        size = mol.sqsize
-        intx = floor(x / size)
-        inty = floor(y / size)
-        intz = floor(z / size)
-        intpx = floor(px / size)
-        intpy = floor(py / size)
-        intpz = floor(pz / size)
-        if (size,intx,inty,intz) != (size,intpx,intpy,intpz):
-            if (size,intpx,intpy,intpz) in self.tree:
-                for i in range((self.tree[size,intpx,intpy,intpz].count(mol))):
-                    self.tree[size,intpx,intpy,intpz].remove(mol)
-                    #print("rem mol:",mol.index,"from:",(size,intpx,intpy,intpz))
-                    if len(self.tree[size,intpx,intpy,intpz]) == 0:
-                        del self.tree[size,intpx,intpy,intpz]
-                        #print("del box:",(size,intpx,intpy,intpz))
-            if (size,intx,inty,intz) not in self.tree:
-                self.tree[size,intx,inty,intz] = []
-            if mol not in self.tree[size,intx,inty,intz]:
-                self.tree[size,intx,inty,intz].append(mol)
 
-                   
-    def query(self,mol):
-        neighbours = []
-        x = mol.loc[0]
-        y = mol.loc[1]
-        z = mol.loc[2]
-        for size in self.sizelist:
-            intx = floor(x / size)
-            inty = floor(y / size)
-            intz = floor(z / size)
-            print(size,intx,inty,intz)
-            if (size,intx,inty,intz) in self.tree:
-                print(size)
-                for i in self.ngrid:
-                    ngridcoord = (size,intx + i[0],inty + i[1],intz + i[2])
-                    if ngridcoord in self.tree:
-                        neighbours.extend(self.tree[ngridcoord])
-        return neighbours
+class KDTree():
+    root_node = "node on top"
+    full_list = []
+    result = []
+    def create(self,parlist,name,depth = 0):
+        if not parlist:
+            return None
         
- 
+        k = len(parlist[0].loc)
+        axis = depth % k
+        parlist.sort(key= lambda p: p.loc[axis])
+        median = int(len(parlist) / 2)
+        node = Nodes()
+        node.name = name
+        if parlist and depth == 0:
+            self.full_list = parlist
+            self.root_node = node
+        node.particle = parlist[median]
+        node.left_child = self.create(parlist[:median],"left" + str(depth+1),depth + 1)
+        node.right_child = self.create(parlist[median + 1:],"right" + str(depth+1),depth + 1)
+        return node
+    
+    def rnn_query(self,point,dist):
+        self.result = []
+        if self.root_node == None:
+            return []
+        else:
+            dist = dist**2
+            print("   distance to reach:",dist)
+            k = len(self.root_node.particle.loc)
+            self.rnn_search(self.root_node,point,dist,k)
+            
+        return self.result
+            
+    def rnn_search(self,node,point,dist,k,depth = 0):
+        
+        if node == None:
+            return
+        
+        axis = depth % k
+        print("    " + node.name + " viewed at " + str(square_dist(point,node.particle.loc,k)**0.5))
+        if square_dist(point,node.particle.loc,k) < dist:
+            print("    " + node.name + " added")
+            self.result.append(node.particle)
+            self.rnn_search(node.left_child,point,dist,k)
+            self.rnn_search(node.right_child,point,dist,k)
+        
+        else:    
+            if point[axis] <= node.particle.loc[axis]:
+                self.rnn_search(node.left_child,point,dist,k)
+            else:
+                self.rnn_search(node.right_child,point,dist,k)
+
+
+    def brute_query(self,point,dist):
+        self.result = []
+        k = len(point)
+        dist = dist**2
+        for i in self.full_list:
+            if square_dist(i.loc,point,k) < dist:
+                self.result.append(i)
+        return self.result
+
+
+
+class Nodes():
+    def __init__(self):
+        name = "node name"
+        loc = [0,0,0]
+        particle = "particle"
+        self.left_child = None
+        self.right_child = None
+   
+        
 def sq_number(val):
     nearsq = 8
     while val > nearsq or val < nearsq / 2:
@@ -175,166 +182,11 @@ def sq_number(val):
             nearsq = nearsq * 2
         elif val < nearsq / 2:
             nearsq = nearsq / 2
-    return nearsq
-         
-'''
-class HashTree:
-    def __init__(self):
-        self.sizelist = []
-        self.tree={}
-        self.ngrid = []
-        rrange = 1
-        for xgrid in range(-rrange,rrange + 1):
-            for ygrid in range(-rrange,rrange + 1):
-                for zgrid in range(-rrange,rrange + 1):
-                    self.ngrid.append((xgrid,ygrid,zgrid))
-        #print(self.ngrid)
-        #print(len(self.ngrid))
-        #print("Gridsize at:",self.gridsize)
-       
-    def add_point(self,mol):
-        x = mol.loc[0]
-        y = mol.loc[1]
-        z = mol.loc[2]
-        size = mol.sqsize
-        if size not in self.sizelist:
-            self.sizelist.append(size)
-        intx = floor(x / size)
-        inty = floor(y / size)
-        intz = floor(z / size)
-        if (size,intx,inty,intz) not in self.tree:
-            self.tree[size,intx,inty,intz] = []
-        if mol not in self.tree[size,intx,inty,intz]:
-            self.tree[size,intx,inty,intz].append((mol.id,mol.sqsize))
-            #print (x,y,z,";",intx,inty,intz)
-           
-    def remove_point(self,mol):
-        x = mol.loc[0]
-        y = mol.loc[1]
-        z = mol.loc[2]
-        size = mol.sqsize
-        intx = floor(x / size)
-        inty = floor(y / size)
-        intz = floor(z / size)
-        if (size,intx,inty,intz) in self.tree:
-            for i in range((self.tree[size,intx,inty,intz].count(mol))):
-                if (size,intx,inty,intz) in self.tree:
-                    self.tree[size,intx,inty,intz].remove(mol)
-                       
-    def update(self,prev_loc,mol):
-        x = mol.loc[0]
-        y = mol.loc[1]
-        z = mol.loc[2]
-        px = prev_loc[0]
-        py = prev_loc[1]
-        pz = prev_loc[2]
-        size = mol.sqsize
-        intx = floor(x / size)
-        inty = floor(y / size)
-        intz = floor(z / size)
-        intpx = floor(px / size)
-        intpy = floor(py / size)
-        intpz = floor(pz / size)
-        if (size,intx,inty,intz) != (size,intpx,intpy,intpz):
-            if (size,intpx,intpy,intpz) in self.tree:
-                for i in range((self.tree[size,intpx,intpy,intpz].count(mol))):
-                    self.tree[size,intpx,intpy,intpz].remove(mol)
-                    #print("rem mol:",mol.index,"from:",(size,intpx,intpy,intpz))
-                    if len(self.tree[size,intpx,intpy,intpz]) == 0:
-                        del self.tree[size,intpx,intpy,intpz]
-                        #print("del box:",(size,intpx,intpy,intpz))
-            if (size,intx,inty,intz) not in self.tree:
-                self.tree[size,intx,inty,intz] = []
-            if mol not in self.tree[size,intx,inty,intz]:
-                self.tree[size,intx,inty,intz].append(mol)
+    return nearsq        
 
-                   
-    def query(self,mol,size):
-        neighbours = []
-        x = mol.loc[0]
-        y = mol.loc[1]
-        z = mol.loc[2]
-        for size in self.sizelist:
-            intx = floor(x / size)
-            inty = floor(y / size)
-            intz = floor(z / size)
-            if (size,intx,inty,intz) in self.tree:
-                for i in self.ngrid:
-                    ngridcoord = (size,intx + i[0],inty + i[1],intz + i[2])
-                    if ngridcoord in self.tree:
-                        neighbours.extend(self.tree[ngridcoord])
-            return neighbours
-
-
-
-        
-def ApplyCns():
+def square_dist(point1,point2,k):
+    sq_dist = 0
+    for i in range(k):
+        sq_dist += (point1[i] - point2[i])**2
+    return sq_dist
     
-    global dctCns
-    global vMass
-    global vBrokenTotal
-
-    vBrokenCns = []
-
-    for ii in dctCns:
-        
-        #print (dctCns[ii][2])
-        
-        Loc1 = dctCns[ii][0].location
-        Loc2 = dctCns[ii][1].location
-        
-        Length=(Loc2-Loc1).length
-        tLength=(trunc(Length,vPrec)*(0.1**vPrec))
-        
-        InitLength = dctCns[ii][2]
-        
-        vStiff=(dctCns[ii][4])
-        
-        if tLength != InitLength:
-            
-            V1=dctCns[ii][0].velocity
-            V2=dctCns[ii][1].velocity
-            
-            LengthX=Loc2[0]-Loc1[0]
-            LengthY=Loc2[1]-Loc1[1]
-            LengthZ=Loc2[2]-Loc1[2]
-            
-            Vx=V2[0]-V1[0]
-            Vy=V2[1]-V1[1]
-            Vz=V2[2]-V1[2]
-            
-            V=(Vx*LengthX+Vy*LengthY+Vz*LengthZ)/Length
-            
-            ForceSpring=(Length-InitLength)*vStiff
-            
-            ForceDamper=vDamping*V
-            
-            ForceX=(ForceSpring+ForceDamper)*LengthX/Length
-            ForceY=(ForceSpring+ForceDamper)*LengthY/Length
-            ForceZ=(ForceSpring+ForceDamper)*LengthZ/Length
-            
-            Force1=[ForceX,ForceY,ForceZ]
-            Force2=[-ForceX,-ForceY,-ForceZ]
-            
-            ApplyForce(dctCns[ii][0],Force1)
-            ApplyForce(dctCns[ii][1],Force2)
-            #print(dctCns[ii][3])
-            #print(vLimitMin+dctCns[ii][3])
-            if tLength > (InitLength*(vLimitMax-dctCns[ii][3])) or tLength < (InitLength*(vLimitMin+dctCns[ii][3])):
-                vBrokenTotal = vBrokenTotal+1
-                vBrokenCns.append(ii)
-                
-    RemoveCns(vBrokenCns)
-    vBrokenCns=[]
-
-def ApplyForce(obj,force):
-    
-    vVel1 = obj.velocity    
-    
-    vVel2 = ((mathu.Vector(force)*vMass)*(vTimeStep*2)) + mathu.Vector(vVel1)
-    
-    obj.velocity = vVel2
-
-
-    return
-'''
