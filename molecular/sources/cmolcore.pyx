92ceabb46166a7f8f34484ea599e5b3e9fda34e8
JF <pyroevil@hotmail.com>
2013-02-26 16:28:39 -0500

    partial implementation of self collision
    not working because instance variable is not shared

--- molecular/sources/cmolcore.pyx
+++ molecular/sources/cmolcore.pyx
@@ -32,6 +32,7 @@ cpdef init(importdata):
     psys = <ParSys *>malloc( psysnum *cython.sizeof(ParSys) )
     cdef int jj = 0
     for i in xrange(psysnum):
+        psys[i].id = i
         psys[i].parnum = importdata[i+1][0]
         parnum += psys[i].parnum
         psys[i].particles = <Particle *>malloc( psys[i].parnum *cython.sizeof(Particle) )
@@ -71,6 +72,10 @@ cpdef init(importdata):
             psys[i].relink_broken = importdata[i + 1][6][21]
             psys[i].relink_brokenrand = importdata[i + 1][6][22]
             psys[i].particles[ii].sys = psys[i]
+            psys[i].particles[ii].collided_with = <Particle *>malloc( 1 *cython.sizeof(Particle) )
+            psys[i].particles[ii].collided_num = 0
+            psys[i].particles[ii].link_with = <Particle *>malloc( 1 *cython.sizeof(Particle) )
+            psys[i].particles[ii].link_withnum = 0
             jj += 1
             
     jj = 0
@@ -81,7 +86,7 @@ cpdef init(importdata):
             jj += 1
     KDTree_create_nodes(kdtree,parnum)
     KDTree_create_tree(kdtree,parlist,0,parnum - 1,"root",0)
-    
+    """
     print("RootNode:",kdtree.root_node[0].index)
     for i in xrange(parnum):
         print("Parent",kdtree.nodes[i].index,"Particle:",kdtree.nodes[i].particle[0].id)
@@ -101,7 +106,7 @@ cpdef init(importdata):
         print("Query Node:",b[i].index)
         print("Query Particle:",b[i].particle[0].id)
     print("number of particle find:",kdtree.num_result)
-    
+    """
     return parnum
 
     
@@ -116,6 +121,17 @@ cpdef simulate(importdata):
     cdef int ii
     
     update(importdata)
+    jj = 0
+    for i in xrange(psysnum):
+        for ii in xrange(psys[i].parnum):
+            parlist[jj] = psys[i].particles[ii]
+            jj += 1
+    KDTree_create_tree(kdtree,parlist,0,parnum - 1,"root",0)
+    
+    for i in xrange(parnum):
+        print(parlist[i].id)
+        print("Z:",parlist[i].loc[2])
+        collide(parlist[i])
     
     exportdata = []
     parloc = []
@@ -138,6 +154,183 @@ cpdef simulate(importdata):
     
     return exportdata
 
+cdef collide(Particle par):
+    global kdtree
+    cdef Node *neighbours
+    cdef Particle par2
+    cdef float stiff
+    cdef float target
+    cdef float sqtarget
+    cdef float lenghtx
+    cdef float lenghty
+    cdef float lenghtz
+    cdef float sqlenght
+    cdef float lenght
+    cdef float factor
+    cdef float ratio1
+    cdef float ratio2
+    cdef float factor1
+    cdef float factor2
+    cdef float col_normal1[3]
+    cdef float col_normal2[3]
+    cdef float ypar_vel[3]
+    cdef float xpar_vel[3]
+    cdef float yi_vel[3]
+    cdef float xi_vel[3]
+    cdef float friction
+    cdef int i
+    print("point0")
+    neighbours = KDTree_rnn_query(kdtree,par.loc,par.size * 2)
+    print("point0.1")
+    for i in xrange(kdtree.num_result):
+        print("point1.1")
+        par2 = neighbours[i].particle[0]
+        if par.id == par2.id:
+            return
+        if arraysearch(par2,par.collided_with,par.collided_num) == -1: 
+        #if par2 not in par.collided_with:
+            print("point1.2")
+            if par2.sys.id != par.sys.id :
+                print("point1.3")
+                if par2.sys.othercollision_active == False or par.sys.othercollision_active == False:
+                    print("point1.4")
+                    return
+            #print(par.sys.collision_group)
+            if par2.sys.collision_group != par.sys.collision_group:
+                print("point2.0")
+                return
+            if par2.sys.id == par.sys.id and par.sys.selfcollision_active == False:
+                print("point3.0")
+                return
+            print("point4.0")
+            stiff = (fps * (substep +1))
+            target = (par.size + par2.size) * 0.99
+            sqtarget = target**2
+            print("point5.0")
+            if par.state <= 1 and par2.state <= 1 and arraysearch(par2,par.link_with,par.link_withnum) == -1 and arraysearch(par,par2.link_with,par2.link_withnum) == -1:
+            #if par.state <= 1 and par2.state <= 1 and par2 not in par.link_with and par not in par2.link_with:
+                print("point6.0")
+                lenghtx = par.loc[0] - par2.loc[0]
+                lenghty = par.loc[1] - par2.loc[1]
+                lenghtz = par.loc[2] - par2.loc[2]
+                print("Z:",par.loc[2] , par2.loc[2])
+                sqlenght  = square_dist(par.loc,par2.loc,3)
+                print("point6.1")
+                print(sqlenght,sqtarget)
+                if sqlenght != 0 and sqlenght < sqtarget:
+                    print("Hit!!!")
+                    lenght = sqlenght**0.5
+                    factor = (lenght - target) / lenght
+                    ratio1 = (par2.mass/(par.mass + par2.mass))
+                    ratio2 = (par.mass/(par.mass + par2.mass))
+                    
+                    par.vel[0] -= (lenghtx * factor * ratio1) * stiff
+                    par.vel[1] -= (lenghty * factor * ratio1) * stiff
+                    par.vel[2] -= (lenghtz * factor * ratio1) * stiff
+                    par2.vel[0] += (lenghtx * factor * ratio2) * stiff
+                    par2.vel[1] += (lenghty * factor * ratio2) * stiff
+                    par2.vel[2] += (lenghtz * factor * ratio2) * stiff
+                    
+                    print("point6.2")
+                    
+                    col_normal1[0] = (par2.loc[0] - par.loc[0]) / lenght
+                    col_normal1[1] = (par2.loc[1] - par.loc[1]) / lenght
+                    col_normal1[2] = (par2.loc[2] - par.loc[2]) / lenght
+                    #print("par2.vel:",par2.vel)
+                    #print("Normal",col_normal1)
+                    #print("Normal_length",(col_normal1[0]**2 + col_normal1[1]**2 + col_normal1[2]**2)**0.5)
+                    col_normal2[0] = col_normal1[0] * -1
+                    col_normal2[1] = col_normal1[1] * -1
+                    col_normal2[2] = col_normal1[2] * -1
+                     
+                    factor1 = dot_product(par.vel,col_normal1)      
+                    ypar_vel[0] = factor1 * col_normal1[0]
+                    ypar_vel[1] = factor1 * col_normal1[1]
+                    ypar_vel[2] = factor1 * col_normal1[2]
+                    xpar_vel[0] = par.vel[0] - ypar_vel[0]
+                    xpar_vel[1] = par.vel[1] - ypar_vel[1]
+                    xpar_vel[2] = par.vel[2] - ypar_vel[2]
+                    print("point6.3")
+                    factor2 = dot_product(par2.vel,col_normal2)
+                    yi_vel[0] = factor2 * col_normal2[0]
+                    yi_vel[1] = factor2 * col_normal2[1]
+                    yi_vel[2] = factor2 * col_normal2[2]
+                    xi_vel[0] = par2.vel[0] - yi_vel[0]
+                    xi_vel[1] = par2.vel[1] - yi_vel[1]
+                    xi_vel[2] = par2.vel[2] - yi_vel[2]
+                    
+                    #print("yi_vel:",yi_vel)
+                    #print("xi_vel:",xi_vel)
+                    
+                    """
+                    Ua = factor1
+                    #print("Ua:",Ua)       
+                    Ub = -factor2
+                    #print("Ub:",Ub)  
+                    Cr = 1
+                    Ma = par.mass
+                    Mb = par2.mass     
+                    Va = (Cr*Mb*(Ub-Ua)+Ma*Ua+Mb*Ub)/(Ma+Mb)
+                    Vb = (Cr*Ma*(Ua-Ub)+Ma*Ua+Mb*Ub)/(Ma+Mb)
+                    #print("Va:",Va)
+                    #print("Vb:",Vb)  
+                    
+                    #print("factor:",-(factor * ratio1) * stiff)
+                    mula = 1
+                    #print("mula:",mula)
+                    mulb = 1
+                    #print("mulb:",mulb)
+                    ypar_vel[0] = col_normal1[0] * Va * mula
+                    ypar_vel[1] = col_normal1[1] * Va * mula
+                    ypar_vel[2] = col_normal1[2] * Va * mula
+                    yi_vel[0] = col_normal1[0] * Vb * mulb
+                    yi_vel[1] = col_normal1[1] * Vb * mulb
+                    yi_vel[2] = col_normal1[2] * Vb * mulb
+                    #print("yi_vel after:",yi_vel)
+                    #print("xi_vel after:",xi_vel)
+                    """
+                    print("point6.4")
+                    friction = 1 - ((par.sys.friction + par2.sys.friction ) / 2)
+                    xpar_vel[0] *= friction
+                    xpar_vel[1] *= friction
+                    xpar_vel[2] *= friction
+                    xi_vel[0] *= friction
+                    xi_vel[1] *= friction
+                    xi_vel[2] *= friction
+                    
+                    #print("par_vel befor:",par.vel)
+                    #print("i_vel befor:",par2.vel)
+                    par.vel[0] = ypar_vel[0] + xpar_vel[0]
+                    par.vel[1] = ypar_vel[1] + xpar_vel[1]
+                    par.vel[2] = ypar_vel[2] + xpar_vel[2]
+                    par2.vel[0] = yi_vel[0] + xi_vel[0]
+                    par2.vel[1] = yi_vel[1] + xi_vel[1]
+                    par2.vel[2] = yi_vel[2] + xi_vel[2]
+                    #print("par_vel after:",par.vel)
+                    #print("i_vel after:",par2.vel)
+                    print("point6.5")
+                    par2.vel[1] += 1.0
+                    par.vel[1] -= 1.0
+                    """
+                    if abs(Va) < abs(((factor * ratio1) * stiff)):
+                        par.vel[0] -= ((lenghtx * factor * ratio1) * stiff)
+                        par.vel[1] -= ((lenghty * factor * ratio1) * stiff)
+                        par.vel[2] -= ((lenghtz * factor * ratio1) * stiff)
+                    if abs(Vb) < abs(((factor * ratio2) * stiff)):
+                        par2.vel[0] += ((lenghtx * factor * ratio2) * stiff)
+                        par2.vel[1] += ((lenghty * factor * ratio2) * stiff)
+                        par2.vel[2] += ((lenghtz * factor * ratio2) * stiff)
+                    """
+                    
+                    #par2.collided_with.append(par)
+                    #if (par.sys.relink_chance + par.sys.relink_chance / 2) > 0:
+                        #create_link(par,[par2,lenght**2])
+
+                    print("point8.0")
+                print("point9.0")
+            print("point10.0")
+        print("point11.0")
+    print("point12.0")
     
 cdef update(data):
     global parlist
@@ -164,6 +357,7 @@ cdef update(data):
             else:
                 psys[i].particles[ii].state = data[i][2][ii]
             psys[i].particles[ii].collided_num = 0
+            print("UpdateZ",psys[i].particles[ii].loc[2])
     
  
 cdef KDTree_create_nodes(KDTree *kdtree,int parnum):
@@ -313,6 +507,7 @@ cdef struct Node:
     
 
 cdef struct ParSys:
+    int id
     int parnum
     Particle *particles
     int selfcollision_active
@@ -350,7 +545,9 @@ cdef struct Particle:
     float state
     ParSys sys
     Particle *collided_with
+    Particle *link_with
     int collided_num
+    int link_withnum
  
  
 cdef int compare_x (const void *u, const void *v):
@@ -387,6 +584,14 @@ cdef int compare_id (const void *u, const void *v):
         return 1
     return 0   
 
+    
+cdef int arraysearch(Particle element,Particle *array,int len):
+    cdef int i
+    for i in xrange(len):
+        if element.id == array[i].id:
+            return i
+    return -1
+    
    
 cdef float sq_number(float val):
     cdef float nearsq = 8
@@ -403,3 +608,8 @@ cdef float square_dist(float point1[3],float point2[3],int k):
     for i in xrange(k):
         sq_dist += (point1[i] - point2[i])**2
     return sq_dist
+    
+cdef float dot_product(float u[3],float v[3]):
+    cdef float dot
+    dot = (u[0] * v[0]) + (u[1] * v[1]) + (u[2] * v[2])
+    return dot
