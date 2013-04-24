8ab049c688548921ea9687c5a14e6594b994d4e0
JF <pyroevil@hotmail.com>
2013-04-24 14:48:38 -0300

    example of batch file pour MS SDK 7.0 64bit
    with manifest line

---
+++ molecular/sources/setup64.bat
@@ -0,0 +1,3 @@
+D:\Python33_x64\python.exe" setup64.py build_ext --inplace
+
+"C:\Program Files\Microsoft SDKs\Windows\v7.0\Bin\mt.exe" -inputresource:D:\stuff\blender-2.66a-windows64\python33.dll;#2 -outputresource:D:\sandbox\blender_db\new_substep_molecular\cmolcore.pyd;#2
