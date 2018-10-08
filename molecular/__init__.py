5ccd791507ea5385c5805d3c0aa02f28e1a3577d
PavelBlend <stalkermodkytia@yandex.ru>
2018-10-08 21:56:25 +0400

    1.0.5

--- molecular/__init__.py
+++ molecular/__init__.py
@@ -19,7 +19,7 @@
 bl_info = {
     "name": "Molecular script",
     "author": "Jean-Francois Gallant(PyroEvil), Pavel_Blend",
-    "version": (1, 0, 4),
+    "version": (1, 0, 5),
     "blender": (2, 7, 9),
     "location": "Properties editor > Particles Tabs",
     "description": ("Molecular script"),
