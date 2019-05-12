0eae152b6d9600bd4bf2ff2b0aefe5805b6074a6
scorpion81 <martin.felke@googlemail.com>
2019-05-12 13:23:51 +0200

    adapt setup.py for mac (and linux)
    Note, under mac it is not so trivial to use openmp, so omitting here for now.

--- sources/setup.py
+++ sources/setup.py
@@ -8,6 +8,7 @@ import Cython.Compiler.Options
 Cython.Compiler.Options.annotate = True
 
 bit_depth = platform.architecture()[0]
+os_name = platform.architecture()[1]
 
 if sys.version_info.major == 3 and sys.version_info.minor == 5 and bit_depth == '64bit':
     module_name = 'core_35_64'
@@ -20,8 +21,11 @@ elif sys.version_info.major == 3 and sys.version_info.minor == 7 and bit_depth =
 else:
     raise BaseException('Unsupported python version')
 
-ext_modules = [Extension(module_name, ['core' + '.pyx'],extra_compile_args=['/Ox','/openmp','/GT','/arch:SSE2','/fp:fast'])]
-
+if os_name == "WindowsPE":
+    ext_modules = [Extension(module_name, ['core' + '.pyx'],extra_compile_args=['/Ox','/openmp','/GT','/arch:SSE2','/fp:fast'])]
+else:
+    #ext_modules = [Extension(module_name, ['core' + '.pyx'],extra_compile_args=['-O3','-fopenmp','-msse4.2','-ffast-math'])]
+    ext_modules = [Extension(module_name, ['core' + '.pyx'],extra_compile_args=['-O3','-msse4.2','-ffast-math'])]
 setup(
 name = 'Molecular script',
 cmdclass = {'build_ext': build_ext},
