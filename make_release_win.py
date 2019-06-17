90986134b75158310c96d0a91e980b81249483b1
Martin Felke <martin.felke@googlemail.com>
2019-06-17 18:28:20 +0200

    attempt to automate windows build, too

--- make_release_win.py
+++ make_release_win.py
@@ -1,12 +1,39 @@
-
 from molecular import bl_info
 from zipfile import ZipFile, ZIP_DEFLATED
-from os import path, walk
+from os import path, walk, remove, chdir, getcwd
+import shutil
+from subprocess import Popen, PIPE
 
+chdir(getcwd()+"\\sources")
 
-with ZipFile('molecular_{}_win'.format(('.'.join(map(str, bl_info['version'])))) + '.zip', 'w') as z:
-    for root, _, files in walk('molecular'):
-        for file in files:
-            if not file.endswith('.py') and not file.endswith('.pyd'):
-                continue
-            z.write(path.join(root, file), compress_type=ZIP_DEFLATED)
+try:
+    remove("core_37_64.cp37-win_amd64.pyd")
+    remove("core.html")
+    remove("core.c")
+    shutil.rmtree("build")
+except:
+    pass
+
+#TODO, blenders (or a compatible) python bin needs to be in $PATH, and if you use blender's you need to copy the python includes from SVN
+#into the include folder of blenders python, too
+
+with Popen(["python3.7", "setup.py", "build_ext", "--inplace"], stdout=PIPE) as proc:
+    print(proc.stdout.read())
+   
+    shutil.move("core_37_64.cp37-win_amd64.pyd", "..\\molecular\\core_37_64.cp37-win_amd64.pyd")
+
+    chdir("..")
+
+    with ZipFile('molecular_{}_'.format(('.'.join(map(str, bl_info['version'])))) + 'win.zip', 'w') as z:
+        for root, _, files in walk('molecular'):
+            for file in files:
+                if not file.endswith('.py') and not file.endswith('.pyd'):
+                    continue
+                z.write(path.join(root, file), compress_type=ZIP_DEFLATED)
+
+    chdir(getcwd()+"\\molecular")
+    try: 
+        remove("core_37_64.cp37-win_amd64.pyd")
+    except:
+        pass
+    chdir("..")
--- sources/setup.py
+++ sources/setup.py
@@ -1,5 +1,6 @@
 import sys
 import platform
+import setuptools
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
