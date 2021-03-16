23f233898623a46f904d26c3c3562fe4de421d0d
u3dreal <48415446+u3dreal@users.noreply.github.com>
2021-03-16 17:51:31 +0100

    Update setup.py

--- c_sources/setup.py
+++ c_sources/setup.py
@@ -31,7 +31,7 @@ elif os_name == "linux":
         module_name,
         ['core' + '.pyx'],
         extra_compile_args=['-O3', '-msse4.2', '-ffast-math', '-fno-builtin','-fopenmp','-static'],
-        extra_link_args=['-lm','-fopenmp','static']
+        extra_link_args=['-lm','-fopenmp','-static']
     )]
 else:
     ext_modules = [Extension(
