409fea6cc71394ad4ec6c370379cfe69f08f8821
u3dreal <48415446+u3dreal@users.noreply.github.com>
2021-05-12 01:06:19 +0200

    Update addon_prefrences.py

--- addon_prefrences.py
+++ addon_prefrences.py
@@ -3,19 +3,21 @@ from os.path import basename, dirname
 from bpy.types import AddonPreferences
 from bpy.props import IntProperty
 
+
 class MolecularAddonPreferences(AddonPreferences):
     bl_idname = __package__
 
     log_size: IntProperty(
-        name = 'Onscreen status size',
-        default = 50,
-        min = 1,
-        max = 200
+        name='Onscreen status size',
+        default=50,
+        min=1,
+        max=200
     )
 
     def draw(self, context):
         layout = self.layout
         row = layout.row()
         row.prop(self, "log_size")
+
 
 pref_classes = (MolecularAddonPreferences)
