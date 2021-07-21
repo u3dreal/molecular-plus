604e86eac4eccb4e4f1382650be9914e9f17c247
u3dreal <48415446+u3dreal@users.noreply.github.com>
2021-07-21 14:46:37 +0200

    test for arm64

--- c_sources/setup.py
+++ c_sources/setup.py
@@ -29,7 +29,7 @@ elif os_name == "Darwin":
     ext_modules = [Extension(
         module_name,
         ['core' + '.pyx'],
-        extra_compile_args=['-march=x86-64','-msse4.2', '-O3','-ffast-math','-fopenmp'],
+        extra_compile_args=['-march=arm64','-msse4.2', '-O3','-ffast-math','-fopenmp'],
         extra_link_args=['-static','-lm','-fopenmp']
     )]
 
