120ed7b808e22d428c73f1ccdccbf8814d7efc7a
u3dreal <48415446+u3dreal@users.noreply.github.com>
2022-07-17 16:31:31 +0200

    update version to 1.19

--- c_sources/core.pyx
+++ c_sources/core.pyx
@@ -45,7 +45,7 @@ cdef Particle *parlist = NULL
 cdef SParticle *parlistcopy = NULL
 cdef ParSys *psys = NULL
 cdef KDTree *kdtree = NULL
-print("cmolcore imported with success! v1.16")
+print("cmolcore imported with success! v1.19")
 
 
 cpdef init(importdata):
