73c136783c3aa8316135dc4d46517c13f6c47840
u3dreal <48415446+u3dreal@users.noreply.github.com>
2023-12-24 16:06:22 +0100

    v1.14.3

--- c_sources/core.pyx
+++ c_sources/core.pyx
@@ -47,7 +47,7 @@ cdef Particle *parlist = NULL
 cdef SParticle *parlistcopy = NULL
 cdef ParSys *psys = NULL
 cdef KDTree *kdtree = NULL
-print("cmolcore imported  v1.14.1")
+print("cmolcore imported  v1.14.3")
 
 
 cpdef init(importdata):
