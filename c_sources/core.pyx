089258031ddfae83f3fcf25ec45d399280142dbd
omid ghotbi (TAO) officially Seyedhosein Ghotbidezuli <omidt.gh@gmail.com>
2024-04-30 22:40:04 +0200

    Delete core.pyx

--- c_sources/core.pyx
+++
@@ -1,9 +0,0 @@
-#cython: profile=False
-#cython: boundscheck=False
-#cython: wraparound=False
-#cython: cdivision=True
-#cython: language_level=3
-#cython: cpow=True
-
-# NOTE: order of slow functions to be optimize/multithreaded:
-# kdtreesearching, kdtreecreating, linksolving
