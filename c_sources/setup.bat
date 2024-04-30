62de1da9d00e2026dbc9b75376beafa7520b6aad
omid ghotbi (TAO) officially Seyedhosein Ghotbidezuli <omidt.gh@gmail.com>
2024-04-30 22:21:32 +0200

    Update setup.bat

--- c_sources/setup.bat
+++ c_sources/setup.bat
@@ -1,21 +1,8 @@
-rem delete old files
-del core.html
-del core.c
-rd /s /q build
-del *.pyd
-
 rem compiling
 python setup.py build_ext --inplace
 @REM "D:\Program Files\Blender Foundation\blender-4.0.2\4.0\python\bin\python.exe" setup.py build_ext --inplace
 
-rem delete unnecessary files after compilation
-del core.html
-del core.c
-rd /s /q build
-
 rem move the core to the blender addon folder
-xcopy /S /Q /Y /F "*.pyd" "%AppData%\Blender Foundation\Blender\4.1\scripts\addons\molecularplus\"
-@REM xcopy /S /Q /Y /F "*.pyd" "%AppData%\Blender Foundation\Blender\4.0\scripts\addons\molecularplus\"
+xcopy /S /Q /Y /F "*.pyd" "%appdata%\Blender Foundation\Blender\4.1\scripts\addons\molecularplus\"
 
-del *.pyd
-pause
+@REM pause
