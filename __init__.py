1582645660df08ccc1255205ba2dcdb05a7a9a1a
u3dreal <48415446+u3dreal@users.noreply.github.com>
2024-07-15 09:02:18 +0200

    v1.17.20

--- __init__.py
+++ __init__.py
@@ -20,7 +20,7 @@ bl_info = {
     "author":
         "Jean-Francois Gallant (PyroEvil), "
         "Gregor Quade (u3dreal)",
-    "version": (1, 17, 19),
+    "version": (1, 17, 20),
     "blender": (3, 00, 0),
     "location": "Properties editor > Physics Tab",
     "description":
