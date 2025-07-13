9df2159f867bab6aefd58a6f6df6f5641d1c512e
u3dreal <48415446+u3dreal@users.noreply.github.com>
2025-07-13 02:06:59 +0200

    Update setup.py

--- c_sources/setup.py
+++ c_sources/setup.py
@@ -12,6 +12,9 @@ os_name = platform.system()
 # Check if Build exists then remove it
 if os.path.exists('build'):
     shutil.rmtree('build')
+if os.path.exists('molecular_core'):
+    shutil.rmtree('molecular_core')
+    shutil.rmtree('molecular_core.egg-info')
 
 # Check if compiled core.* exists then remove it
 if os.path.isfile('core.html'):
@@ -19,10 +22,19 @@ if os.path.isfile('core.html'):
 if os.path.isfile('core.c'):
     os.remove('core.c')
 
+os.mkdir("./molecular_core")
+shutil.copy("__init__.py", "./molecular_core/__init__.py")
+# Copy libomp.dylib to molecular_plus_core/ if needed
+if os_name == "Darwin":
+    shutil.copyfile(
+        "./openmp/x86_64/lib/libomp.dylib",
+        "./molecular_core/libomp.dylib"
+    )
+
 filenames = ['simulate.pyx', 'update.pyx', 'init.pyx', 'links.pyx', 'kdtree.pyx',
              'collide.pyx', 'memory.pyx', 'utils.pyx', 'structures.pyx']
 
-with open('core.pyx', 'w') as outfile:
+with open('./molecular_core/core.pyx', 'w') as outfile:
     # This will clear the file before writing to it
     outfile.truncate(0)
     for fname in filenames:
@@ -31,61 +43,66 @@ with open('core.pyx', 'w') as outfile:
             outfile.write('\n')  # Add a newline character at the end of each file
 
 Cython.Compiler.Options.annotate = True
-module_name = 'core'
+module_name = 'molecular_core.core'
 
 if not DEBUG_MODE:
     if os_name == "Windows":
         ext_modules = [Extension(
             module_name,
-            ['core.pyx'],
+            ['molecular_core/core.pyx'],
             extra_compile_args=['/Ox','/openmp','/GT','/arch:SSE2','/fp:fast']#['/Ox','/openmp:llvm','/GT','/arch:SSE2','/fp:fast', '/wd4244', '/MD']
         )]
 
     elif os_name == "Linux":
         ext_modules = [Extension(
             module_name,
-            ['core.pyx'],
+            ['molecular_core/core.pyx'],
             extra_compile_args=['-O3', '-msse4.2', '-ffast-math', '-fno-builtin','-fopenmp'],
             extra_link_args=['-lm','-fopenmp']
         )]
     elif os_name == "Darwin":
         ext_modules = [Extension(
             module_name,
-            ['core.pyx'],
+            ['molecular_core/core.pyx'],
             extra_compile_args=['-msse4.2', '-O3', '-ffast-math', '-fno-builtin', '-arch', 'x86_64', '-Xclang', '-fopenmp', '-isystem./openmp/x86_64/include'],
-            extra_link_args=['-lm', '-L./openmp/x86_64/lib', '-lomp', '-arch', 'x86_64']
+            extra_link_args=['-lm', '-L./openmp/x86_64/lib', '-lomp', '-arch', 'x86_64', '-Wl,-rpath,@loader_path']
         )]
 else:
     if os_name == "Windows":
         ext_modules = [Extension(
             module_name,
-            ['core.pyx'],
+            ['molecular_core/core.pyx'],
             extra_compile_args=['/O0','/openmp','/GT','/arch:SSE2','/fp:fast']#['/Zi', '/O0','/openmp:llvm', '/MD']
         )]
 
     elif os_name == "Linux":
         ext_modules = [Extension(
             module_name,
-            ['core.pyx'],
+            ['molecular_core/core.pyx'],
             extra_compile_args=['-O3', '-g','-fopenmp'],
             extra_link_args=['-lm','-fopenmp']
         )]
     elif os_name == "Darwin":
         ext_modules = [Extension(
             module_name,
-            ['core.pyx'],
+            ['molecular_core/core.pyx'],
             extra_compile_args=['-msse4.2', '-O0', '-g', '-arch', 'x86_64', '-Xclang', '-fopenmp', '-isystem./openmp/x86_64/include'],
-            extra_link_args=['-lm', '-L./openmp/x86_64/lib', '-lomp', '-arch', 'x86_64']
+            extra_link_args=['-lm', '-L./openmp/x86_64/lib', '-lomp', '-arch', 'x86_64', '-Wl,-rpath,@loader_path']
         )]
 
 setup(
-    name = 'molecular_plus_core',
+    name = 'molecular_core',
     version = core_version,
-    ext_modules = cythonize(ext_modules
-    )
+    ext_modules=cythonize(ext_modules),
+    packages=["molecular_core"],
+    package_data={"molecular_core": ["libomp.dylib"]} if os_name == "Darwin" else [],
+    zip_safe=False,
 )
 
 # Delete the files after the build
+if os_name == "Darwin" and os.path.exists("molecular_core/libomp.dylib"):
+    os.remove("molecular_core/libomp.dylib")
+
 if os.path.isfile('core.html'):
     os.remove('core.html')
 if os.path.isfile('core.c'):
