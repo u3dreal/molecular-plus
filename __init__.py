12e463ea245d3053c922b12371ffaee145d5d847
u3dreal <48415446+u3dreal@users.noreply.github.com>
2025-10-24 22:11:27 +0200

    v1.21.5

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@
 bl_info = {
     "name": "Molecular+",
     "author": "Gregor Quade (u3dreal)",
-    "version": (1, 21, 4),
+    "version": (1, 21, 5),
     "blender": (4, 2, 0),
     "location": "Properties editor > Physics Tab",
     "description": "Addon for calculating collisions "
--- c_sources/simulate.pyx
+++ c_sources/simulate.pyx
@@ -53,7 +53,7 @@ cdef Particle *parlist = NULL
 cdef SParticle *parlistcopy = NULL
 cdef ParSys *psys = NULL
 cdef SpatialHash *spatialhash = NULL
-print("cmolcore imported  v1.21.2")
+print("cmolcore imported  v1.21.5")
 
 cpdef simulate(importdata):
     global spatialhash
